f4a251c390d05e15c0942dd028ec580a7953becc
IlyasDiker <ilyasbenhssine@gmail.com>
2020-11-16 19:56:13 +0100

    add delete_task

--- Todo/tasks/views.py
+++ Todo/tasks/views.py
@@ -34,3 +34,13 @@ def updateTask(request, pk):
 
     context = {'form': form}
     return render(request, 'tasks/update_task.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item':item}
+    return render(request, 'tasks/delete.html', context)

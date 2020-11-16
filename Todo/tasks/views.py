c91d17e545ac2f1ff0fd42e0459d1c5c812c303f
IlyasDiker <ilyasbenhssine@gmail.com>
2020-11-16 18:59:13 +0100

    add view & Visualise Tasks

--- Todo/tasks/views.py
+++ Todo/tasks/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/list.html')
+    tasks = Task.objects.all()
+
+    context = {'tasks':tasks}
+    return render(request, 'tasks/list.html', context)

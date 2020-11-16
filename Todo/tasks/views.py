34d787e5750cf8bdd931e856b7cd7c9735253d61
IlyasDiker <ilyasbenhssine@gmail.com>
2020-11-16 19:10:33 +0100

    add ModelForm for Task

--- Todo/tasks/views.py
+++ Todo/tasks/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
+
 
 # Create your views here.
 def index(request):
     tasks = Task.objects.all()
 
-    context = {'tasks':tasks}
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
+    context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
